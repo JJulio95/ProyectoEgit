fa5f3415a98cee2af45eeb1c5bf5e14a2f2252dc
JJulio95 <jcammar773@g.educaand.es>
2021-11-08 19:12:28 +0100

    primera version 1

---
+++ Principal11.java
@@ -0,0 +1,16 @@
+package ProyectoGIT;
+import java.util.*;
+
+public class Principal11 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int numero1, numero2, resultado;
+		System.out.print("Introduce primer número: ");
+		numero1 = sc.nextInt();
+		System.out.print("Introduce segundo número: ");
+		numero2 = sc.nextInt();
+
+	}
+
+}
